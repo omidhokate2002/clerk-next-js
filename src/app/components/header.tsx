825460a0a9c9559b98547d96ad2249b9c22fcbe3
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-03-05 00:51:25 +0530

    Header and UserInfo Imported

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,16 +1,25 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-const Header = () => {
+const Header = async () => {
+  const { userId } = auth();
   return (
     <div className="bg-gray-600 text-neutral-100">
       <div className="container mx-auto flex items-center justify-between py-4">
         <Link href="/">Home</Link>
-        <div className="flex gap-4">
-          <UserButton />
-          <Link href="/sign-up">Sign Up</Link>
-          <Link href="/sign-in">Sign In</Link>
+        <div>
+          {userId ? (
+            <div className="flex gap-4 items-center">
+              <Link href="/dashbord">Dashboard</Link>
+              <UserButton afterSignOutUrl="/" />
+            </div>
+          ) : (
+            <div className="flex gap-4 items-center">
+              <Link href="/sign-up">Sign Up</Link>
+              <Link href="/sign-in">Sign In</Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
