82bc06a5d92d5848a42b40e64adb40244e002f5a
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-03-05 13:04:54 +0530

    Charts Using Chart.js for admin and home page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { auth, currentUser } from "@clerk/nextjs";
+import RadarChart from "../components/radar";
 
 export default async function DashboardPage() {
   const { userId } = auth();
@@ -9,30 +10,35 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="mt-10 text-start max-w-xl mx-auto bg-gray-900 rounded-lg overflow-hidden shadow-xl">
-      <div className="bg-gradient-to-r from-purple-500 to-indigo-600 px-6 py-4">
-        <p className="text-sm text-white">
-          Welcome to your personalized dashboard!
-        </p>
+    <div className="flex">
+      <div className="w-1/2 mt-10 text-start max-w-xl mx-auto bg-gray-900 rounded-lg overflow-hidden shadow-xl">
+        <div className="bg-gradient-to-r from-purple-500 to-indigo-600 px-6 py-4">
+          <p className="text-sm text-white">
+            Welcome to your personalized dashboard!
+          </p>
+        </div>
+        <div className="px-6 py-8">
+          <h1 className="text-4xl font-bold text-white mb-4">Welcome</h1>
+          <ul className="list-none space-y-4">
+            <li className="flex items-center justify-between">
+              <span className="font-semibold text-white">First Name:</span>{" "}
+              <span className="text-blue-400">{user.firstName}</span>
+            </li>
+            <li className="flex items-center justify-between">
+              <span className="font-semibold text-white">Last Name:</span>{" "}
+              <span className="text-blue-400">{user.lastName}</span>
+            </li>
+            <li className="flex items-center justify-between">
+              <span className="font-semibold text-white">Email:</span>{" "}
+              <span className="text-blue-400">
+                {user.emailAddresses[0].emailAddress}
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="px-6 py-8">
-        <h1 className="text-4xl font-bold text-white mb-4">Welcome</h1>
-        <ul className="list-none space-y-4">
-          <li className="flex items-center justify-between">
-            <span className="font-semibold text-white">First Name:</span>{" "}
-            <span className="text-blue-400">{user.firstName}</span>
-          </li>
-          <li className="flex items-center justify-between">
-            <span className="font-semibold text-white">Last Name:</span>{" "}
-            <span className="text-blue-400">{user.lastName}</span>
-          </li>
-          <li className="flex items-center justify-between">
-            <span className="font-semibold text-white">Email:</span>{" "}
-            <span className="text-blue-400">
-              {user.emailAddresses[0].emailAddress}
-            </span>
-          </li>
-        </ul>
+      <div className="w-1/2 max-w-xl mx-auto rounded-lg">
+        <RadarChart />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import LineChart from "./components/lineChart";
 
 export default function Home() {
   return (
     <div>
       <h1 className="font-bold mt-10 text-center">Home Page</h1>
+      <div>
+        <LineChart />
+      </div>
     </div>
   );
 }
